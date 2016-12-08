3094b1f4844b13c04529a5e22c95852f20d77374
dandi <deliterjob@gmail.com>
2016-12-08 11:57:38 +0100

    error near where but it is the last thing to do

--- src/control/CatalogueController.java
+++ src/control/CatalogueController.java
@@ -237,19 +237,19 @@ public class CatalogueController {
 
         System.out.println(sql);
         ArrayList<Article> articoli =  UserDatabase.getInstance().searchArticle(sql);
-        for (int i = 0; i < articoli.size(); i++){
-            if(rq.getClass().equals(Book.class)){
-                sql = "SELECT * FROM ARTICLES.libro JOIN ARTICLES.articolo WHERE UPPER(ARTICLES.libro.NOME) LIKE UPPER('" + articoli.get(i).getNome() + "') AND UPPER(ARTICLES.libro.NOME) = UPPER(ARTICLES.libro.articolo)";
-                UserDatabase.getInstance().extendedQuery(sql, "Book");
-            }
-            else if(rq.getClass().equals(Electronics.class)){
-
-            }
-            else if(rq.getClass().equals(Clothing.class)){
-
-            }
-            else if(rq.getClass().equals(TextBook.class)){
-
+        for (Article a : articoli) {
+            if (rq.getClass().equals(Book.class)) {
+                sql = "SELECT * FROM ARTICLES.libro JOIN ARTICLES.articolo WHERE UPPER(ARTICLES.libro.NOME) LIKE UPPER('" + a.getNome() + "') AND UPPER(ARTICLES.libro.NOME) = UPPER(ARTICLES.articolo.NOME)";
+                a = UserDatabase.getInstance().extendedQuery(sql, "Book");
+            } else if (rq.getClass().equals(Electronics.class)) {
+                sql = "SELECT * FROM ARTICLES.informatica JOIN ARTICLES.articolo WHERE UPPER(ARTICLES.informatica.NOME) LIKE UPPER('" + a.getNome() + "') AND UPPER(ARTICLES.libro.NOME) = UPPER(ARTICLES.articolo.NOME)";
+                a = UserDatabase.getInstance().extendedQuery(sql, "Electronics");
+            } else if (rq.getClass().equals(Clothing.class)) {
+                sql = "SELECT * FROM ARTICLES.Abbigliamento JOIN ARTICLES.articolo WHERE UPPER(ARTICLES.Abbigliamento.NOME) LIKE UPPER('" + a.getNome() + "') AND UPPER(ARTICLES.Abbigliamento.NOME) = UPPER(ARTICLES.articolo.NOME)";
+                a = UserDatabase.getInstance().extendedQuery(sql, "Clothing");
+            } else if (rq.getClass().equals(TextBook.class)) {
+                sql = "SELECT * FROM ARTICLES.Scolastico JOIN ARTICLES.articolo WHERE UPPER(ARTICLES.Scolastico.NOME) LIKE UPPER('" + a.getNome() + "') AND UPPER(ARTICLES.Scolastico.NOME) = UPPER(ARTICLES.articolo.NOME)";
+                a = UserDatabase.getInstance().extendedQuery(sql, "TextBook");
             }
         }
 
--- src/control/DatabaseController.java
+++ src/control/DatabaseController.java
@@ -1,7 +1,7 @@
 package control;
 
 import databaseINIT.Provider;
-import entity.Article;
+import entity.*;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -40,5 +40,48 @@ abstract class DatabaseController {
         return array;
     }
 
-
+    Article extendedQuery(String sql, String kind) throws SQLException {
+        Article nuovoArticolo = null;
+        ResultSet rs = null;
+        try {
+            Statement stmt = provider.getConnection().createStatement();
+            rs = stmt.executeQuery(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        if (rs != null) {
+            if(rs.next())
+            {
+                nuovoArticolo = new Article();
+                nuovoArticolo.setNome(rs.getString("NOME"));
+                nuovoArticolo.setProprietario(rs.getString("PROPRIETARIO"));
+                nuovoArticolo.setPrezzo(rs.getFloat("PREZZO"));
+                nuovoArticolo.setQuantità(rs.getInt("QUANTITA"));
+                switch (kind) {
+                    case "Book":
+                        ((Book) nuovoArticolo).setTitolo(rs.getString("TITOLO"));
+                        ((Book) nuovoArticolo).setAutore(rs.getString("AUTORE"));
+                        ((Book) nuovoArticolo).setEditore(rs.getString("CASA"));
+                        ((Book) nuovoArticolo).setPagine(rs.getInt("PAGINE"));
+                        break;
+                    case "Electronics":
+                        ((Electronics) nuovoArticolo).setModello(rs.getString("MODELLO"));
+                        ((Electronics) nuovoArticolo).setMarca(rs.getString("MARCA"));
+                        break;
+                    case "Clothing":
+                        ((Clothing) nuovoArticolo).setTaglia(rs.getInt("TAGLIA"));
+                        ((Clothing) nuovoArticolo).setTipo(rs.getString("TIPO"));
+                        ((Clothing) nuovoArticolo).setMarca(rs.getString("MARCA"));
+                        break;
+                    case "TextBook":
+                        ((TextBook) nuovoArticolo).setEdizione(rs.getInt("EDIZIONE"));
+                        ((TextBook) nuovoArticolo).setMateria(rs.getString("MATERIA"));
+                        break;
+                    default:
+                        return null;
+                }
+            }
+        }
+        return nuovoArticolo;
+    }
 }
